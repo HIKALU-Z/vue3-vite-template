c3bcf82d107f1fc9eb165d4ea840bf5b8a2bee25
hikalu <play3a@126.com>
2022-06-28 09:54:41 +0800

    chore: improve husky flow

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,8 @@ import eslint from 'vite-plugin-eslint'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), eslint({
-  // 配置选项
+    // 配置选项
+    cache: false // 禁用 eslint 缓存
+    // fix: false
   })]
 })
