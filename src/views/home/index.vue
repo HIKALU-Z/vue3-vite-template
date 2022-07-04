cb5772c4a9414ccc83d9550fbd9d12e253d03ac6
HIKALU <hikalu@zhangguanlindeMacBook-Pro.local>
2022-07-04 23:48:24 +0800

    chore: add alias @ for path

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,9 +1,14 @@
 <template>
-  <h1>首页</h1>
+  <h1>{{ data }}</h1>
+  <img
+    src="@/assets/logo.png"
+    alt=""
+    srcset=""
+  >
 </template>
 
 <script setup lang="ts">
-
+import data from '@/api'
 </script>
 
 <style lang="scss" scoped>
